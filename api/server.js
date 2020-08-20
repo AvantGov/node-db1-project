7a9715b31e96102b54d703e908f37e55490a3437
Alex <alexcouts18@gmail.com>
2020-08-19 20:07:31 -0500

    intial commit with some basic boiler plate architecting and planning on the routing file

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const application_router = require("./Routers/application_router")
+
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
-
+server.use("/", application_router)
 module.exports = server;
