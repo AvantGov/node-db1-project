7fd2add63498d7122ba1317b614b53ec0581814c
Alex <alexcouts18@gmail.com>
2020-08-22 12:15:07 -0500

    MVP met with SQL portion of MVP trascked in the read me document

--- api/Routers/application_router.js
+++ api/Routers/application_router.js
@@ -4,23 +4,58 @@ const accounts_access = require("../../data/dbConfig")
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try {
+        const accounts = await accounts_access.select('*').from("accounts")
+        res.json(accounts)
+    } catch(error) { 
+        next(error) 
+    }
+})
 
-// - find all customers with postal code 1010. Returns 3 records.
-router.get()
+router.post('/', async (req, res, next) => {
+    try {
+        const account = await accounts_access
+            .insert({
+                name: req.body.name,
+                budget: req.body.budget
+            })
+            .into("accounts")
+        res.status(201).json(account)
+    } catch(error) { 
+        next(error) 
+    }
+})
 
-// - find the phone number for the supplier with the id 11. Should be (010) 9984510.
-router.get()
+router.put("/:id", async (req, res, next) => {
+    try {
+        await accounts_access("accounts")
+            .update({
+                name: req.body.name,
+                budget: req.body.budget
+            })
+            .where("id", req.params.id)
+
+            res.status(201).json({ message: "resource update successful!" })
+    } catch(error) { 
+        next(error) 
+    }
 
 
-// - list first 10 orders placed, sorted descending by the order date. The order with date 1997-02-12 should be at the top.
-router.get()
+})
 
-// - find all customers that live in London, Madrid, or Brazil. Returns 18 records.
+router.delete("/:id", async (req,res, next) => {
+    try {
+        await accounts_access('accounts')
+            .where("id", req.params.id)
+            .del()
 
-// - add a customer record for _"The Shire"_, the contact name is _"Bilbo Baggins"_ the address is _"1 Hobbit-Hole"_ in _"Bag End"_, postal code _"111"_ and the country is _"Middle Earth"_.
+        res.status(204).end()
+    } catch(error) { 
+        next(error) 
+    }
 
-// - update _Bilbo Baggins_ record so that the postal code changes to _"11122"_.
-
+})
 
 
 module.exports = router
